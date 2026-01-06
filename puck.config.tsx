b7db67a7569f488b7b898cf6be2b9373210b161e
javijec <javijec@gmail.com>
2026-01-06 15:55:26 -0300

    INIT

---
+++ puck.config.tsx
@@ -0,0 +1,71 @@
+import { DropZone, type Config } from "@measured/puck";
+
+type Props = {
+  HeadingBlock: { title: string };
+  GridBlock: {};
+  CardBlock: {
+    title: string;
+    description: string;
+    padding: number;
+    variant: string;
+  };
+};
+
+export const config: Config<Props> = {
+  components: {
+    HeadingBlock: {
+      fields: {
+        title: { type: "text" },
+      },
+      defaultProps: {
+        title: "Heading",
+      },
+      render: ({ title }) => (
+        <div className="text-4xl font-bold p-4">
+          <h1>{title}</h1>
+        </div>
+      ),
+    },
+    GridBlock: {
+      fields: {
+        columns: { type: "number" },
+      },
+      defaultProps: {
+        columns: 3,
+      },
+      render: () => {
+        return (
+          <DropZone zone="my-grid" className="grid grid-cols-3 gap-4 p-4" />
+        );
+      },
+    },
+    CardBlock: {
+      fields: {
+        title: { type: "text" },
+        description: { type: "textarea" },
+        padding: { type: "number" },
+        variant: {
+          type: "select",
+          options: [
+            { label: "Outline", value: "border rounded-md" },
+            { label: "Floating", value: "shadow-md" },
+          ],
+        },
+      },
+      defaultProps: {
+        title: "Card",
+        description: "Description",
+        padding: 16,
+        variant: "border rounded-md",
+      },
+      render: ({ title, description, padding, variant }) => (
+        <div style={{ padding }} className={`${variant}`}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+      ),
+    },
+  },
+};
+
+export default config;
